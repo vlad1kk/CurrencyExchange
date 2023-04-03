ade3ed02ac994fd720926123c84681a8ebfd6eb9
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-04-03 13:10:50 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@ const currencyValue = document.querySelector('.currency');
 const jsOutput = document.querySelector('.js-output');
 
 jsInput.addEventListener('input', function(){
-    console.log()
+    // console.log(jsInput.value)
     const uah = jsInput.value;
     const currency = currencyValue.value;
-})
-
-function name(){
-    const jsInput.value;
-
-    const result = 
+    const result = convert(uah, currency);
 
     jsOutput.innerText = result;
-}
+})
+currencyValue.addEventListener('change', function(){
+    const uah = jsInput.value;
+    const currency = currencyValue.value;
+    const result = convert(uah, currency);
+
+    jsOutput.innerText = result;
+})
