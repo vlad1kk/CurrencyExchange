7cd9cfc039299bd6ccf489b91c4225f417cde808
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-04-03 13:19:00 +0300

    Optimized

--- js/currency.js
+++ js/currency.js
@@ -1,9 +1,9 @@
 const RATES = {
-    usd: 0.026350461133069828,
-    eur: 0.024330900243309
+    usd: 0.026,
+    eur: 0.024
 }
 
-function convert (uah, currency){
+function convert ({uah, currency}){
     if(!RATES[currency]){
         return null;
     }
--- js/script.js
+++ js/script.js
@@ -2,18 +2,24 @@ const jsInput = document.querySelector('.js-input');
 const currencyValue = document.querySelector('.currency');
 const jsOutput = document.querySelector('.js-output');
 
+function getInput(){
+    return{
+        uah: Number(jsInput.value),
+        currency: currencyValue.value
+    }
+}
+
+function render(result){
+    jsOutput.innerText = result;
+}
+
 jsInput.addEventListener('input', function(){
     // console.log(jsInput.value)
-    const uah = jsInput.value;
-    const currency = currencyValue.value;
-    const result = convert(uah, currency);
-
-    jsOutput.innerText = result;
+    const result = convert(getInput());
+    render(result);
+    
 })
 currencyValue.addEventListener('change', function(){
-    const uah = jsInput.value;
-    const currency = currencyValue.value;
-    const result = convert(uah, currency);
-
-    jsOutput.innerText = result;
+    const result = convert(getInput());
+    render(result);
 })
